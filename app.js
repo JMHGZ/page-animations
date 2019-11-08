5fc1950aaa6527efe63946e219365ece9e6692cc
Jeremiah Garza <jag_tt@yahoo.com>
2019-11-07 22:18:59 -0600

    more CSS/darkened bg, starting on js selectors

---
+++ app.js
@@ -0,0 +1,5 @@
+const mtn = document.querySelector(".mtn");
+const slider = document.querySelector(".slider");
+const logo = document.querySelector("#logo");
+const hamburger = document.querySelector(".hamburger");
+const tagline = document.querySelector(".tagline");
